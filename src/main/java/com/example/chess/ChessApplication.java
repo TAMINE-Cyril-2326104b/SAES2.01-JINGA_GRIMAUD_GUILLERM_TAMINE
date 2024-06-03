b55813152e17b55efcd2057702251aabd216c3d8
Cyril <cyril.tamine@etu.univ-amu.fr>
2024-06-03 14:59:45 +0200

    Modification de la scène

--- src/main/java/com/example/chess/ChessApplication.java
+++ src/main/java/com/example/chess/ChessApplication.java
@@ -3,6 +3,7 @@ package com.example.chess;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,9 +11,9 @@ import java.io.IOException;
 public class ChessApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(ChessApplication.class.getResource("ChessMainPage.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(ChessApplication.class.getResource("/fxml/ChessMainPage.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Chess.com");
         stage.setScene(scene);
         stage.show();
     }
