eb903e618a410480973fbe5a2d5429f47d6f7ac5
Cyril <cyril.tamine@etu.univ-amu.fr>
2024-06-03 21:55:24 +0200

    Ajout d'un changement de page après démarrage de la partie.

---
+++ src/main/java/com/example/chess/controllers/ChessMainPageController.java
@@ -0,0 +1,30 @@
+package com.example.chess.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+
+
+public class ChessMainPageController {
+
+
+    public void handleChangeScene(ActionEvent event) throws IOException{
+        Parent secondSceneParent = FXMLLoader.load(getClass().getResource("/fxml/ChessBotGame.fxml"));
+        Scene secondScene = new Scene(secondSceneParent);
+
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(secondScene);
+        stage.show();
+    }
+
+
+
+
+
+}
