6777e9534d3992cb755824bc15f273836b8055e0
GUILLERM Mathis <GUILLERM@gmail.com>
2024-06-03 18:31:41 +0200

    3/06 apparence

--- src/main/java/com/example/chess/ChessApplication.java
+++ src/main/java/com/example/chess/ChessApplication.java
@@ -6,13 +6,13 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
-import java.io.IOException;
 
+import java.io.IOException;
 public class ChessApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(ChessApplication.class.getResource("/fxml/ChessMainPage.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 700, 450);
         stage.setTitle("Chess.com");
         stage.setScene(scene);
         stage.show();
