062cd0defc04b64a1a78cde78d23e565b14d09e3
GUILLERM Mathis <GUILLERM@gmail.com>
2024-06-04 12:12:34 +0200

    modif fxml et main mais marche pas doit continuer

--- src/main/java/com/example/chess/ChessApplication.java
+++ src/main/java/com/example/chess/ChessApplication.java
@@ -3,17 +3,26 @@ package com.example.chess;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 
 import java.io.IOException;
 public class ChessApplication extends Application {
+    //private GridPane gridPane;
+
+//    public ChessApplication(GridPane gridPane) {
+//        this.gridPane = gridPane;
+//    }
+
     @Override
     public void start(Stage stage) throws IOException {
+
+        //Plateau plateau=new Plateau(gridPane);
+
         FXMLLoader fxmlLoader = new FXMLLoader(ChessApplication.class.getResource("/fxml/ChessMainPage.fxml")); // On charge le fichier FXML.
         Scene scene = new Scene(fxmlLoader.load(), 700, 450); // On crée notre scène à partir du fichier FXML et on définit sa taille.
         stage.setTitle("Chess.com"); // On définit le titre de notre interface.
+
         stage.setScene(scene);
         stage.show();
     }
